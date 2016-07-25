e3439db4d4b20f749adc015eb7effd28f32f2c36
chuckboris <ulicny.boris@gmail.com>
2016-07-25 16:46:15 +0200

    Resolved crashes with compiler optimization set to "Fast"
    Refactored code into one line, which solved crashes with compiler optimization set to "Fast"

--- Font+MonospacedDigits.swift
+++ Font+MonospacedDigits.swift
@@ -23,22 +23,8 @@
 import UIKit
 
 extension UIFont {
-    
-    var monospacedDigitFont: UIFont {
-        let oldFontDescriptor = fontDescriptor()
-        let newFontDescriptor = oldFontDescriptor.monospacedDigitFontDescriptor
-        return UIFont(descriptor: newFontDescriptor, size: 0)
-    }
-    
-}
 
-private extension UIFontDescriptor {
-    
-    var monospacedDigitFontDescriptor: UIFontDescriptor {
-        let fontDescriptorFeatureSettings = [[UIFontFeatureTypeIdentifierKey: kNumberSpacingType, UIFontFeatureSelectorIdentifierKey: kMonospacedNumbersSelector]]
-        let fontDescriptorAttributes = [UIFontDescriptorFeatureSettingsAttribute: fontDescriptorFeatureSettings]
-        let fontDescriptor = self.fontDescriptorByAddingAttributes(fontDescriptorAttributes)
-        return fontDescriptor
+    var monospacedDigitFont: UIFont {
+        return UIFont(descriptor: fontDescriptor().fontDescriptorByAddingAttributes([UIFontDescriptorFeatureSettingsAttribute: [[UIFontFeatureTypeIdentifierKey: kNumberSpacingType, UIFontFeatureSelectorIdentifierKey: kMonospacedNumbersSelector]]]), size: 0)
     }
-    
 }
